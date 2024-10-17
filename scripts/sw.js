a3eb041535692663e235eb8d7c344584b142e0c4
Danylo <warfais6@gmail.com>
2024-10-17 19:47:58 +0300

    Add PWA

---
+++ scripts/sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'my-site-cache-v1'
+const urlsToCache = [
+  '/',
+  '/assets/css/style.css',
+  '/assets/css/response.css',
+  '/scripts/script.js',
+  '/assets/img/сайт.png'
+]
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Opened cache')
+      return cache.addAll(urlsToCache)
+    })
+  )
+})
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request)
+    })
+  )
+})
